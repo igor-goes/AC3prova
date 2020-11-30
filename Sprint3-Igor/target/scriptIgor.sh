c88af8e9101611db0143735c28bb5bb7b4f499ce
igor-goes <igor.nascimento@bandtec.com.br>
2020-11-30 20:11:43 -0300

    Agora vai 1000x

---
+++ Sprint3-Igor/target/scriptIgor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Verificando se possue java"
+
+which java | grep /bin/java
+
+if [[ $? = 0 ]]
+	then
+	echo "Java encontrado"
+	echo "Executando arquivo do projeto"
+	java -jar projeto-ac3-1.0-SNAPSHOT-jar-with-dependencies.jar
+else	
+	echo "Java não encontrado"
+	echo "Instalando o java"
+	sudo apt install curl
+	curl -s "https://get.sdkman.io" | bash
+	source "$HOME/.sdkman/bin/sdkman-init.sh"
+	sdk install java 8.0.275.j9-adpt
+	echo "Executando arquivo do projeto"
+	java -jar projeto-ac3-1.0-SNAPSHOT-jar-with-dependencies.jar	 	
+fi
+
+
